ca7a7beca53507c79783dc0cf382b368113119b6
Anh Duc Phan <jacksonp.se1@gmail.com>
2024-01-17 14:44:56 +1000

    Done L4.2

--- L4. Hooks/2. State and Side Effects/Starter/src/App.js
+++ L4. Hooks/2. State and Side Effects/Starter/src/App.js
@@ -1,33 +1,33 @@
 import logo from "./logo.svg";
 import "./App.css";
-
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
+import { useState, useEffect } from "react";
+import Game from "./Game";
+import Score from "./Score";
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">ReactND - Coding Practice</h1>
-      </header>
-      <div className="game">
-        <h2>Mental Math</h2>
-        <div className="equation">
-          <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
-        </div>
-        <button>True</button>
-        <button>False</button>
-        <p className="text">
-          Your Score: {numCorrect}/{numQuestions}
-        </p>
-      </div>
-    </div>
-  );
+	const [corrAnswer, setCorrAnswer] = useState(0);
+	const [numQuestions, setNumQuestions] = useState(0);
+
+	const handleAnswer = (answerCorr) => {
+		if (answerCorr) {
+			setCorrAnswer(corrAnswer + 1);
+		}
+		setNumQuestions(numQuestions + 1);
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<h1 className="App-title">ReactND - Coding Practice</h1>
+			</header>
+			<div className="game">
+				<h2>Mental Math</h2>
+				<Game handleAnswer={handleAnswer} />
+				<Score numCorrect={corrAnswer} numQuestions={numQuestions} />
+			</div>
+		</div>
+	);
 };
 
 export default App;
--- L4. Hooks/2. State and Side Effects/Starter/src/Game.js
+++ L4. Hooks/2. State and Side Effects/Starter/src/Game.js
@@ -4,15 +4,14 @@ const Game = (props) => {
 	const [value1, setValue1] = useState(null);
 	const [value2, setValue2] = useState(null);
 	const [value3, setValue3] = useState(null);
-	const [proposedAnswer, setProposedAnswer] = useState(null);
+	const [trueAnswer, setTrueAnswer] = useState(null);
 
-	// Calls makeNewQuestion() when the component has been mounted, then sets values
 	useEffect(() => {
 		let valuesArray = makeNewQuestion();
 		setValue1(valuesArray[0]);
 		setValue2(valuesArray[1]);
 		setValue3(valuesArray[2]);
-		setProposedAnswer(valuesArray[3]);
+		setTrueAnswer(valuesArray[3]);
 	}, []);
 
 	const makeNewQuestion = () => {
@@ -20,44 +19,44 @@ const Game = (props) => {
 		const value2 = Math.floor(Math.random() * 100);
 		const value3 = Math.floor(Math.random() * 100);
 
-		const proposedAnswer =
+		const trueAnswer =
 			Math.floor(Math.random() * 3) + (value1 + value2 + value3);
 
-		return [value1, value2, value3, proposedAnswer];
+		return [value1, value2, value3, trueAnswer];
 	};
 
 	const refresh = (newValuesArray) => {
 		setValue1(newValuesArray[0]);
 		setValue2(newValuesArray[1]);
 		setValue3(newValuesArray[2]);
-		setProposedAnswer(newValuesArray[3]);
+		setTrueAnswer(newValuesArray[3]);
 	};
 
 	const handleAnswer = (event) => {
 		const newValuesArray = makeNewQuestion();
 		refresh(newValuesArray);
-		const answerWasCorrect = evaluateAnswer(event.target.name);
-		props.handleAnswer(answerWasCorrect);
+		const answerCorr = evalAnswer(event.target.name);
+		props.handleAnswer(answerCorr);
 	};
 
-	const evaluateAnswer = (givenAnswer) => {
+	const evalAnswer = (givenAnswer) => {
 		const corrAnswer = value1 + value2 + value3;
 
 		return (
-			(corrAnswer === proposedAnswer && givenAnswer === "true") ||
-			(corrAnswer !== proposedAnswer && givenAnswer === "false")
+			(corrAnswer === trueAnswer && givenAnswer === "true") ||
+			(corrAnswer !== trueAnswer && givenAnswer === "false")
 		);
 	};
 
 	return (
 		<div>
 			<div className="equation">
-				<p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+				<p className="text">{`${value1} + ${value2} + ${value3} = ${trueAnswer}`}</p>
 			</div>
-			<button onClick={handleAnswer} name="true">
+			<button className="btn btn-success" onClick={handleAnswer} name="true">
 				True
 			</button>
-			<button name="false" onClick={handleAnswer}>
+			<button className="btn btn-danger" name="false" onClick={handleAnswer}>
 				False
 			</button>
 		</div>
